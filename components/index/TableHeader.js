1c8ef447bcc14d6f0fa68fb0c1f5560449583af4
nico23973 <nico.gartmann05@gmail.com>
2022-02-13 20:14:40 +0100

    removed unnecessary stuff

--- components/index/TableHeader.js
+++ components/index/TableHeader.js
@@ -1,16 +1,7 @@
 import styles from "./TableHeader.module.scss";
 import Th from "./Th";
 
-export let uiIsShownData;
-
 function TableHeader(props) {
-	let data = [];
-	props.names.forEach((name) => {
-		data[name] = false;
-	});
-
-	uiIsShownData = data;
-
 	return (
 		<thead className={styles.tableHeader}>
 			<tr>
--- components/index/Th.js
+++ components/index/Th.js
@@ -1,28 +1,7 @@
-import { useState } from "react";
-import { uiIsShownData } from "./TableHeader";
-
 export default function Th(props) {
-	const [uiIsShown, setUiIsShown] = useState(uiIsShownData);
-	const [rand, setRand] = useState(uiIsShownData);
-
-	function toggleUiIsShown(state) {
-		let newValue = uiIsShown;
-		newValue[props.name] = state;
-		setUiIsShown(newValue);
-		setRand(Math.random());
-	}
-
 	return (
-		<th
-			onMouseEnter={() => {
-				toggleUiIsShown(true);
-			}}
-			onMouseLeave={() => {
-				toggleUiIsShown(false);
-			}}
-		>
+		<th>
 			<strong>{props.name}</strong>
-			{uiIsShown[props.name] ? <p>hello</p> : null}
 		</th>
 	);
 }
