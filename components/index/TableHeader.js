c77159d103c008d8d1f36a5b2375825d2e01df38
nico23973 <nico.gartmann05@gmail.com>
2022-02-14 17:37:34 +0100

    added ui elements

--- components/index/TableHeader.js
+++ components/index/TableHeader.js
@@ -1,21 +1,26 @@
 import styles from "./TableHeader.module.scss";
 import Th from "./Th";
-
-export let uiIsShownData;
+import { useState } from "react";
 
 function TableHeader(props) {
 	let data = {};
 	props.names.forEach((name) => {
 		data[name] = false;
 	});
-
-	uiIsShownData = data;
+	let [uiIsShown, setUiIsShown] = useState(data);
 
 	return (
 		<thead className={styles.tableHeader}>
 			<tr>
 				{props.names.map((name, i) => {
-					return <Th name={name} key={i} />;
+					return (
+						<Th
+							name={name}
+							key={i}
+							uiIsShown={uiIsShown}
+							setUiIsShown={setUiIsShown}
+						/>
+					);
 				})}
 			</tr>
 		</thead>
--- components/index/Th.js
+++ components/index/Th.js
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import { uiIsShownData } from "./TableHeader";
+import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Th(props) {
-	const [uiIsShown, setUiIsShown] = useState(uiIsShownData);
-
 	function toggleUiIsShown(state) {
-		uiIsShown[props.name] = state;
-		setUiIsShown({ ...uiIsShown });
+		let old = props.uiIsShown;
+		old[props.name] = state;
+		props.setUiIsShown({ ...old });
 	}
 
 	return (
@@ -17,9 +16,51 @@ export default function Th(props) {
 			onMouseLeave={() => {
 				toggleUiIsShown(false);
 			}}
+			style={{ width: 36 }}
 		>
-			<strong>{props.name}</strong>
-			{uiIsShown[props.name] ? <p>hello</p> : null}
+			<div style={{ position: "relative" }}>
+				{props.uiIsShown[props.name] ? (
+					<div>
+						<div style={{ position: "absolute", bottom: 4, left: "16px" }}>
+							<Image
+								src="/../public/arrUp.svg"
+								alt="Sort up"
+								height="6"
+								width="12"
+							/>
+						</div>
+						<div style={{ position: "absolute", top: 4, left: "16px" }}>
+							<Image
+								src="/../public/arrDown.svg"
+								alt="Sort down"
+								height="6"
+								width="12"
+							/>
+						</div>
+					</div>
+				) : null}
+				{props.name}
+				<div
+					style={{
+						paddingLeft: "6",
+						position: "absolute",
+						right: "16px",
+						bottom: "0",
+					}}
+				>
+					{props.uiIsShown[props.name] ? (
+						<Image
+							height="12"
+							width="12"
+							src="/../public/cross.svg"
+							alt="Delete Column"
+							onClick={() => {
+								console.log("click");
+							}}
+						/>
+					) : null}
+				</div>
+			</div>
 		</th>
 	);
 }
